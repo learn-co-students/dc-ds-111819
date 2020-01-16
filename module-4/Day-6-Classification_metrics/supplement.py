853b973b7ff0b700d5a9c57f3ff6bd9bd30701f1
Murat Guner <murat.guner@flatironschool.com>
2020-01-16 11:01:12 -0500

    classification notebook added

---
+++ module-4/Day-6-Classification_metrics/supplement.py
@@ -0,0 +1,52 @@
+col = [c for c in df.columns if c not in ['target']]
+numclasses=[]
+for c in col:
+    numclasses.append(len(np.unique(df[[c]])))
+
+threshold=10
+categorical_variables = list(np.array(col)[np.array(numclasses)<threshold])
+
+enc = OneHotEncoder( handle_unknown= 'ignore')
+remaining_list = [i for i in df.columns.tolist() if i not in categorical_variables]
+remaining_list.remove('target')
+X_num = X_train[remaining_list].values
+
+X_Cat = enc.fit_transform(X_train[categorical_variables]).toarray()
+X = np.concatenate((X_num, X_Cat), axis = 1)
+
+lr = LogisticRegression(C = 1e9, solver = 'lbfgs', max_iter=10000)
+lr.fit(X, y_train)
+
+cv = StratifiedKFold(n_splits= 5, random_state=1019, shuffle=True)
+
+lr_vanilla = LogisticRegression(C = 1e9,
+                                solver = 'newton-cg',
+                                max_iter=1000)
+
+l2_reg = LogisticRegression(C = 1,
+                            solver = 'newton-cg',
+                            max_iter=1000)
+
+l1_reg = LogisticRegression(C = 1,
+                           solver= 'saga',
+                           penalty = 'l1',
+                            max_iter=1000)
+
+cv_vanilla = cross_validate(estimator=lr_vanilla,
+                            X = X, y = y_train,
+                            cv = cv,
+                            n_jobs=-1,
+                            return_estimator= True,
+                            return_train_score=True)
+
+cv_l2 = cross_validate(estimator=l2_reg, X = X, y = y_train,
+                    cv = cv,
+                    n_jobs=-1,
+                    return_estimator= True,
+                    return_train_score=True)
+
+cv_l1 = cross_validate(estimator=l1_reg, X = X, y = y_train,
+                    cv = cv,
+                    n_jobs=-1,
+                    return_estimator= True,
+                    return_train_score=True)
